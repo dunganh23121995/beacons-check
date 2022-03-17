e11d2553934666427d637abcb80cce8ccb7c6022
Cao Anh Dung <62677973+dunganh23121995@users.noreply.github.com>
2022-03-17 19:26:05 +0700

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,16 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
+Set<String> uuidBeacons = {
+  "16DE7852-34AD-4CA0-AF9C-4FEB41D7F413",
+  "48534442-4c45-4144-80c0-180000000000",
+  "16DE7852-34AD-4CA0-AF9C-100000000000",
+  "D4C3CCC0-29FB-11E5-884F-0002A5D5C51B",
+  "B0702881-A295-A8AB-F734-031A98A50000",
+};
+
 class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-  new FlutterLocalNotificationsPlugin();
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
 
   String _tag = "Beacons Plugin";
   String _beaconResult = 'Not Scanned Yet.';
@@ -29,8 +36,7 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
 
   final ScrollController _scrollController = ScrollController();
 
-  final StreamController<String> beaconEventsController =
-  StreamController<String>.broadcast();
+  final StreamController<String> beaconEventsController = StreamController<String>.broadcast();
 
   @override
   void initState() {
@@ -39,14 +45,11 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
     initPlatformState();
 
     // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
-    var initializationSettingsAndroid =
-    new AndroidInitializationSettings('app_icon');
-    var initializationSettingsIOS =
-    IOSInitializationSettings(onDidReceiveLocalNotification: null);
-    var initializationSettings = InitializationSettings(
-        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
-    flutterLocalNotificationsPlugin.initialize(initializationSettings,
-        onSelectNotification: null);
+    var initializationSettingsAndroid = new AndroidInitializationSettings('app_icon');
+    var initializationSettingsIOS = IOSInitializationSettings(onDidReceiveLocalNotification: null);
+    var initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings, onSelectNotification: null);
   }
 
   @override
@@ -69,54 +72,34 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
       await BeaconsPlugin.setDisclosureDialogMessage(
           title: "Background Locations",
           message:
-          "[This app] collects location data to enable [feature], [feature], & [feature] even when the app is closed or not in use");
+              "[This app] collects location data to enable [feature], [feature], & [feature] even when the app is closed or not in use");
 
       //Only in case, you want the dialog to be shown again. By Default, dialog will never be shown if permissions are granted.
       //await BeaconsPlugin.clearDisclosureDialogShowFlag(false);
     }
+    BeaconsPlugin.listenToBeacons(beaconEventsController);
+    // await BeaconsPlugin.clearRegions();
 
-    if (Platform.isAndroid) {
-      BeaconsPlugin.channel.setMethodCallHandler((call) async {
-        print("Method: ${call.method}");
-        if (call.method == 'scannerReady') {
-          _showNotification("Beacons monitoring started..");
-          await BeaconsPlugin.startMonitoring();
-          setState(() {
-            isRunning = true;
-          });
-        } else if (call.method == 'isPermissionDialogShown') {
-          _showNotification(
-              "Prominent disclosure message is shown to the user!");
-        }
-      });
-    } else if (Platform.isIOS) {
-      _showNotification("Beacons monitoring started..");
-      await BeaconsPlugin.startMonitoring();
-      setState(() {
-        isRunning = true;
-      });
+     // BeaconsPlugin.addRegion("BeaconType1", "16DE7852-34AD-4CA0-AF9C-4FEB41D7F413")
+     //    .then((value) => print("add region success"));
+     // BeaconsPlugin.addRegion("BeaconType2", "48534442-4c45-4144-80c0-180000000000")
+     //    .then((value) => print("add region success"));
+     // BeaconsPlugin.addRegion("BeaconType3", "16DE7852-34AD-4CA0-AF9C-100000000000")
+     //    .then((value) => print("add region success"));
+    for(String uuid in uuidBeacons){
+       BeaconsPlugin.addRegion("BeaconType $uuid", "$uuid")
+          .then((value) => print("add region success"));
+      // await BeaconsPlugin.addRegionForIOS("$uuid", 200, 100, "name");
     }
 
-    BeaconsPlugin.listenToBeacons(beaconEventsController);
+    BeaconsPlugin.setForegroundScanPeriodForAndroid(foregroundScanPeriod: 2200, foregroundBetweenScanPeriod: 10);
 
-    await BeaconsPlugin.addRegion(
-        "BeaconType1", "909c3cf9-fc5c-4841-b695-380958a51a5a");
-    await BeaconsPlugin.addRegion(
-        "BeaconType2", "6a84c716-0f2a-1ce9-f210-6a63bd873dd9");
-
-    BeaconsPlugin.addBeaconLayoutForAndroid(
-        "m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25");
-    BeaconsPlugin.addBeaconLayoutForAndroid(
-        "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24");
-
-    BeaconsPlugin.setForegroundScanPeriodForAndroid(
-        foregroundScanPeriod: 2200, foregroundBetweenScanPeriod: 10);
-
-    BeaconsPlugin.setBackgroundScanPeriodForAndroid(
-        backgroundScanPeriod: 2200, backgroundBetweenScanPeriod: 10);
+    BeaconsPlugin.setBackgroundScanPeriodForAndroid(backgroundScanPeriod: 2200, backgroundBetweenScanPeriod: 10);
 
     beaconEventsController.stream.listen(
-            (data) {
+        (data) {
+          print("Beacons DataReceived: " + data);
+
           if (data.isNotEmpty && isRunning) {
             setState(() {
               _beaconResult = data;
@@ -136,8 +119,8 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
           print("Error: $error");
         });
 
-    //Send 'true' to run in background
-    await BeaconsPlugin.runInBackground(true);
+    // //Send 'true' to run in background
+    // await BeaconsPlugin.runInBackground(true);
 
     if (!mounted) return;
   }
@@ -154,16 +137,29 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              SizedBox(
+                height: 100,
+                child: Scrollbar(
+                  isAlwaysShown: true,
+                  child: ListView(
+                    children: [
+                      Text("List Beacons Support"),
+                      for(String uuid in uuidBeacons.toList())
+                        Text("$uuid"),
+                    ],
+                  ),
+                ),
+              ),
               Center(
                   child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('Total Results: $_nrMessagesReceived',
-                        style: Theme.of(context).textTheme.headline4?.copyWith(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Total Results: $_nrMessagesReceived',
+                    style: Theme.of(context).textTheme.headline4?.copyWith(
                           fontSize: 14,
                           color: const Color(0xFF22369C),
                           fontWeight: FontWeight.bold,
                         )),
-                  )),
+              )),
               Padding(
                 padding: const EdgeInsets.all(2.0),
                 child: ElevatedButton(
@@ -178,8 +174,7 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
                       isRunning = !isRunning;
                     });
                   },
-                  child: Text(isRunning ? 'Stop Scanning' : 'Start Scanning',
-                      style: TextStyle(fontSize: 20)),
+                  child: Text(isRunning ? 'Stop Scanning' : 'Start Scanning', style: TextStyle(fontSize: 20)),
                 ),
               ),
               Visibility(
@@ -193,8 +188,7 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
                         _results.clear();
                       });
                     },
-                    child:
-                    Text("Clear Results", style: TextStyle(fontSize: 20)),
+                    child: Text("Clear Results", style: TextStyle(fontSize: 20)),
                   ),
                 ),
               ),
@@ -212,17 +206,12 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
   void _showNotification(String subtitle) {
     var rng = new Random();
     Future.delayed(Duration(seconds: 5)).then((result) async {
-      var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-          'your channel id', 'your channel name',
-          importance: Importance.high,
-          priority: Priority.high,
-          ticker: 'ticker');
+      var androidPlatformChannelSpecifics = AndroidNotificationDetails('your channel id', 'your channel name',
+          importance: Importance.high, priority: Priority.high, ticker: 'ticker');
       var iOSPlatformChannelSpecifics = IOSNotificationDetails();
-      var platformChannelSpecifics = NotificationDetails(
-          android: androidPlatformChannelSpecifics,
-          iOS: iOSPlatformChannelSpecifics);
-      await flutterLocalNotificationsPlugin.show(
-          rng.nextInt(100000), _tag, subtitle, platformChannelSpecifics,
+      var platformChannelSpecifics =
+          NotificationDetails(android: androidPlatformChannelSpecifics, iOS: iOSPlatformChannelSpecifics);
+      await flutterLocalNotificationsPlugin.show(rng.nextInt(100000), _tag, subtitle, platformChannelSpecifics,
           payload: 'item x');
     });
   }
@@ -243,17 +232,16 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
         ),
         itemBuilder: (context, index) {
           DateTime now = DateTime.now();
-          String formattedDate =
-          DateFormat('yyyy-MM-dd – kk:mm:ss.SSS').format(now);
+          String formattedDate = DateFormat('yyyy-MM-dd – kk:mm:ss.SSS').format(now);
           final item = ListTile(
               title: Text(
                 "Time: $formattedDate\n${_results[index]}",
                 textAlign: TextAlign.justify,
                 style: Theme.of(context).textTheme.headline4?.copyWith(
-                  fontSize: 14,
-                  color: const Color(0xFF1A1B26),
-                  fontWeight: FontWeight.normal,
-                ),
+                      fontSize: 14,
+                      color: const Color(0xFF1A1B26),
+                      fontWeight: FontWeight.normal,
+                    ),
               ),
               onTap: () {});
           return item;
